67a784ecf34f9a5506ab9525d70af5ed46f9c9ba
sina <haleysina@gmail.com>
2024-08-11 20:55:36 +0330

    factorial added

--- hi.py
+++ hi.py
@@ -2,3 +2,14 @@ print("HI!!!!")
 
 
 # coppl!!!
+for item in range(total):
+    # comment: 
+# end for
+
+
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
